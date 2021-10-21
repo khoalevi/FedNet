7c44f860434f46a447d6aaeed57be8759ced7ba5
Khoa Vo <khoa.hyper@gmail.com>
2021-10-21 11:10:12 +0700

    Add Intersection over Union (IoU)

---
+++ fed/iou.py
@@ -0,0 +1,17 @@
+def compute_iou(boxA, boxB):
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+
+    # compute the area of both the prediction and ground-truth boxes
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+    # compute the intersection over union
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    return iou
